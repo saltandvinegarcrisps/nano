8e086fca0e825a2e85e6c33213164132a519611e
rwarasaurus <info@kieronwilson.co.uk>
2012-11-05 15:38:53 +0000

    added default timezone, removed register globals fix as its removed from php 5.4

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -13,7 +13,7 @@ if(ini_get('register_globals')) {
 
 	foreach($globals as $global) {
 		foreach(array_keys($global) as $key) {
-			unset(${$key}); 
+			unset(${$key});
 		}
 	}
 }
@@ -63,7 +63,7 @@ error_reporting(-1);
 /*
 	Localisation - Register the default timezone for the application.
 */
-date_default_timezone_set(Config::get('application.timezone'));
+date_default_timezone_set(Config::get('application.timezone', 'UTC'));
 
 /*
 	Set input
