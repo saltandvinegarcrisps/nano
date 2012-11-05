b1e0c5d42da47879f34fd7fb1060f31b3d111a2c
rwarasaurus <info@kieronwilson.co.uk>
2012-11-05 15:35:03 +0000

    added port

--- app/config/database.php
+++ app/config/database.php
@@ -5,12 +5,13 @@ return array(
 	'default' => 'mysql',
 
 	'fetch' => PDO::FETCH_OBJ,
-	
+
 	'connections' => array(
 
 		'mysql' => array(
 			'driver' => 'mysql',
 			'hostname' => 'localhost',
+			'post' => 3306,
 			'username' => 'root',
 			'password' => '',
 			'database' => '',
