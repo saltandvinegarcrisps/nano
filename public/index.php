fec9ba53c0ce83b4c912d09ee66f1082aec7afff
rwarasaurus <info@kieronwilson.co.uk>
2012-11-12 13:15:57 +0000

    nicer way of getting the env var

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DS', '/');
-define('ENV', (isset($_ENV['APP_ENV']) ? $_ENV['APP_ENV'] : 'production'));
+define('ENV', getenv('APP_ENV'));
 
 define('PATH', dirname(dirname(__FILE__)) . DS);
 define('APP', PATH . 'app' . DS);
